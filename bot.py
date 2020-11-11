211b921fbf30b839cb647b0529d7b111981e7fdd
C4pt4inPh4ntom <ronitmohanty152000@gmail.com>
2020-11-11 20:47:37 +0530

    optmised

--- bot.py
+++ bot.py
@@ -3,13 +3,35 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
 
+f=open("rules.txt","r")
+rules=f.readlines()
+
 @client.event
 async def on_ready():
     print("Bot is up and running !")
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server. Fresh meat has arrived')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server. The prey has left :(')
+
+
 @client.command()
 async def hello(ctx):
     await ctx.send("Fuck off you cunt")
+
+
+@client.command()
+async def cringe(ctx):
+    await ctx.send("Kya rey tera bulla kat dunga")
+
+@client.command()
+async def rule(ctx,*,number): 
+    await ctx.send(rules[int(number)-1])
+
 
 
 
